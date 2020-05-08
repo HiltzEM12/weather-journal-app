2085e62a00b79c020a3b081bfb6e041f3583d244
Eric Hiltz <eric.m.hiltz@gmail.com>
2020-05-08 09:44:32 -0400

    Set up basics for server and js app

--- server.js
+++ server.js
@@ -2,18 +2,61 @@
 projectData = {};
 
 // Require Express to run server and routes
+const express = require('express');
 
 // Start up an instance of app
+const app = express();
 
 /* Middleware*/
 //Here we are configuring express to use body-parser as middle-ware.
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
 app.use(bodyParser.json());
 
 // Cors for cross origin allowance
+const cors = require('cors');
+app.use(cors());
 
 // Initialize the main project folder
 app.use(express.static('website'));
 
 
 // Setup Server
+
+//Define what port to use
+const port = 8000;
+
+//Callback function for the server listener
+function listening() {
+    console.log('Weather Jounal App Server running');
+    console.log(`Running on localhost: ${port}`);
+}
+
+// Initiate a server.  Takes the port and a callback function
+const server = app.listen(port, listening);
+
+// For stroring the data
+const weatherData = []; 
+
+// GET route
+app.get('/weather', function (request, response) {
+    response.send(weatherData);
+    console.log(weatherData);
+});
+
+// POST route
+app.post('/weather', function (request, response) {
+
+    // Get the data
+    newWeather = {
+        temperature: request.body.temperature,
+        date: request.body.date,
+        userResponse: request.body.userResponse
+    }
+
+    // Put the data into the array
+    weatherData.push(newWeather);
+    response.send(weatherData);
+    console.log(weatherData);
+});
--- website/app.js
+++ website/app.js
@@ -1,5 +1,78 @@
 /* Global Variables */
 
+// api url format: api.openweathermap.org/data/2.5/weather?zip={zip code},{country code}&appid={your api key}
+// example api.openweathermap.org/data/2.5/weather?zip=94040,us
+const baseURL = 'api.openweathermap.org/data/2.5/weather?zip=';
+const apiKey = '482619460b0b5529ab4cde59127b0ac2'
+
 // Create a new date instance dynamically with JS
 let d = new Date();
 let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+
+
+// Add listener to the generate button
+document.getElementById('generate').addEventListener('click', displayWeather);
+
+
+
+// Function for the listener
+function displayWeather(event){
+    const zipCode =  document.getElementById('zip').value;
+    const feeling =  document.getElementById('feelings').value;
+  
+    getWeather('/weather') // Async get function written elswhere
+    // New Syntax!
+    .then(function(data){ // Promise chain
+      // Add data
+      console.log(data);
+      // Async post function written elswhere
+      postData('/weather', {temperature:data.temperature, date: data.date, userResponse:feeling} );
+    })
+    .then( // Another promise chain
+      updateUI() //Updat the UI with the data recieved using the function below
+    )
+  }
+
+
+// Weather API response example
+// {
+//     "coord": {"lon": -122.08,"lat": 37.39},
+//     "weather": [
+//       {
+//         "id": 800,
+//         "main": "Clear",
+//         "description": "clear sky",
+//         "icon": "01d"
+//       }
+//     ],
+//     "base": "stations",
+//     "main": {
+//       "temp": 282.55,
+//       "feels_like": 281.86,
+//       "temp_min": 280.37,
+//       "temp_max": 284.26,
+//       "pressure": 1023,
+//       "humidity": 100
+//     },
+//     "visibility": 16093,
+//     "wind": {
+//       "speed": 1.5,
+//       "deg": 350
+//     },
+//     "clouds": {
+//       "all": 1
+//     },
+//     "dt": 1560350645,
+//     "sys": {
+//       "type": 1,
+//       "id": 5122,
+//       "message": 0.0139,
+//       "country": "US",
+//       "sunrise": 1560343627,
+//       "sunset": 1560396563
+//     },
+//     "timezone": -25200,
+//     "id": 420006353,
+//     "name": "Mountain View",
+//     "cod": 200
+//   }

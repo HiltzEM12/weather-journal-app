5855deccd4473e22d8f9830723366d4325e9266b
Eric Hiltz <eric.m.hiltz@gmail.com>
2020-05-08 12:24:44 -0400

    Got the temperature from the API.  Now need to use local server

--- website/app.js
+++ website/app.js
@@ -2,77 +2,103 @@
 
 // api url format: api.openweathermap.org/data/2.5/weather?zip={zip code},{country code}&appid={your api key}
 // example api.openweathermap.org/data/2.5/weather?zip=94040,us
-const baseURL = 'api.openweathermap.org/data/2.5/weather?zip=';
+const baseURL = 'https://api.openweathermap.org/data/2.5/weather?zip=';
 const apiKey = '482619460b0b5529ab4cde59127b0ac2'
+
+// Document fields
+const zipCodeField = document.getElementById('zip')
+const feelingField = document.getElementById('feelings')
 
 // Create a new date instance dynamically with JS
 let d = new Date();
-let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+let newDate = d.getMonth() + '.' + d.getDate() + '.' + d.getFullYear();
 
+
+/* Helper Functions */
+//Wrapper to create the URL to call the weather API
+function createWeatherURL(baseURL, zipCodeField, apiKey) {
+    return baseURL + zipCodeField + ',us&appid=' + apiKey
+}
+
+// Kelvin to fahrenheit
+// Returns temperature rounded to 2 decimals
+function kelToFah(K) {
+    return ((((K - 273.15) * 9) / 5) + 32).toFixed(2);
+}
 
 // Add listener to the generate button
-document.getElementById('generate').addEventListener('click', displayWeather);
+document.getElementById('generate').addEventListener('click', onClick);
 
-
+// Function to handle getting the weather and displaying results
+function onClick(event) {
+    // Only proceed if there are values in the zip code and feelings fields
+    if (zipCodeField && zipCodeField.value && feelingField && feelingField.value) {
+        displayWeather(createWeatherURL(baseURL, zipCodeField.value, apiKey));
+    }
+}
 
 // Function for the listener
-function displayWeather(event){
-    const zipCode =  document.getElementById('zip').value;
-    const feeling =  document.getElementById('feelings').value;
-  
-    getWeather('/weather') // Async get function written elswhere
-    // New Syntax!
-    .then(function(data){ // Promise chain
-      // Add data
-      console.log(data);
-      // Async post function written elswhere
-      postData('/weather', {temperature:data.temperature, date: data.date, userResponse:feeling} );
-    })
-    .then( // Another promise chain
-      updateUI() //Updat the UI with the data recieved using the function below
-    )
-  }
+function displayWeather(url) {
+    // Get the temperature from the weather API using the zip code entered
+    temp = callWeatherAPI(url)
+        .then(
+            // Post the weather and other info to the server
+            postData('/weather', {
+                temperature: temp,
+                date: newDate,
+                userResponse: feelingField.value
+            })
+        ).then(
+            updateUI() //Update the user interface with results and entries
+        )
+}
 
+// Function for handling the API call to the weather site
+// Just returns the temperature and a number
+async function callWeatherAPI(url = '') {
+    const request = await fetch(url);
 
-// Weather API response example
-// {
-//     "coord": {"lon": -122.08,"lat": 37.39},
-//     "weather": [
-//       {
-//         "id": 800,
-//         "main": "Clear",
-//         "description": "clear sky",
-//         "icon": "01d"
-//       }
-//     ],
-//     "base": "stations",
-//     "main": {
-//       "temp": 282.55,
-//       "feels_like": 281.86,
-//       "temp_min": 280.37,
-//       "temp_max": 284.26,
-//       "pressure": 1023,
-//       "humidity": 100
-//     },
-//     "visibility": 16093,
-//     "wind": {
-//       "speed": 1.5,
-//       "deg": 350
-//     },
-//     "clouds": {
-//       "all": 1
-//     },
-//     "dt": 1560350645,
-//     "sys": {
-//       "type": 1,
-//       "id": 5122,
-//       "message": 0.0139,
-//       "country": "US",
-//       "sunrise": 1560343627,
-//       "sunset": 1560396563
-//     },
-//     "timezone": -25200,
-//     "id": 420006353,
-//     "name": "Mountain View",
-//     "cod": 200
-//   }
+    try {
+        //Transform to JSON
+        const data = await request.json();
+        return kelToFah(data.main.temp);
+    } catch (error) {
+        console.log("error", error);
+    }
+}
+
+// Function to post data to the server
+async function postData (url = '', data = {}) {
+
+    const response = await fetch(url, {
+        method: 'POST',
+        credentials: 'same-origin',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data), // body data type must match "Content-Type" header        
+    });
+
+    try {
+        const newData = await response.json();
+        return newData;
+    } 
+    
+    catch (error) {
+        console.log("error", error);
+    }
+};
+
+// Function to update the user interface
+async function updateUI () {
+    const request = await fetch('/weather');
+
+    try {
+        const newData = await response.json();
+        console.log('UI stuff', newData)
+    } 
+
+    catch (error) {
+        console.log("error", error);
+    }
+}
